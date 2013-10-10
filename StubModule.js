65627fb88d8cbdcfee13bab2e33b738f7fdf28e0
Scott Davis <scott.sheri@gmail.com>
2013-10-10 09:50:11 -0600

    Fixed alias removal test.
    Also, Steve's wall-o-pink.

--- StubModule.js
+++ StubModule.js
@@ -1,74 +1,76 @@
 /*jshint unused:false, loopfunc:true*/
 define([
-    'dojo/_base/array',
-    'dojo/_base/lang'
-],
+        'dojo/_base/array',
+        'dojo/_base/lang'
+    ],
 
-function (
-    array,
-    lang
+    function(
+        array,
+        lang
     ) {
-    return function (modulePath, stubs, forcedRefreshModules) {
-        // inspired by: http://stackoverflow.com/questions/11439540/how-can-i-mock-dependencies-for-unit-testing-in-requirejs
-        // and https://github.com/mattfysh/testr.js
-        var aliases = [],
-            key,
-            stubname,
-            returnModule,
-            clonedAliases;
+        return function(modulePath, stubs, forcedRefreshModules) {
+            // inspired by: http://stackoverflow.com/questions/11439540/how-can-i-mock-dependencies-for-unit-testing-in-requirejs
+            // and https://github.com/mattfysh/testr.js
+            var aliases = [],
+                key,
+                stubname,
+                returnModule,
+                clonedAliases;
 
-        require.undef(modulePath);
+            require.undef(modulePath);
 
-        // clear out any modules that need to be forced (usually dependencies that use the stubs)
-        array.forEach(forcedRefreshModules, function (mod) {
-            require.undef(mod);
-        });
+            // clear out any modules that need to be forced (usually dependencies that use the stubs)
+            array.forEach(forcedRefreshModules, function(mod) {
+                require.undef(mod);
+            });
 
-        // add stubs as aliases
-        for (key in stubs) {
-            if (stubs.hasOwnProperty(key)) {
-                // clear any previously cached object for this alias
-                require.undef(key);
+            // add stubs as aliases
+            for (key in stubs) {
+                if (stubs.hasOwnProperty(key)) {
+                    // clear any previously cached object for this alias
+                    require.undef(key);
 
-                stubname = 'STUB_' + key;
+                    stubname = 'STUB_' + key;
 
-                aliases.push([key, stubname]);
+                    aliases.push([key, stubname]);
 
-                define(stubname, [], function () {
-                    return stubs[key];
-                });
+                    define(stubname, [], function() {
+                        return stubs[key];
+                    });
+                }
             }
-        }
 
-        // clone array because passing it in the require config messes with the values
-        clonedAliases = lang.clone(aliases);
+            // clone array because passing it in the require config messes with the values
+            clonedAliases = lang.clone(aliases);
 
-        // get module with stubs
-        require({aliases: clonedAliases}, [modulePath], function (Module) {
-            returnModule = Module;
-        });
+            // get module with stubs
+            require({
+                aliases: clonedAliases
+            }, [modulePath], function(Module) {
+                returnModule = Module;
+            });
 
-        // remove stub aliases
-        var removedIndexs = [];
-        array.forEach(require.aliases, function (al, i) {
-            if (array.some(clonedAliases, function (cAl) {
-                return al[0] === cAl[0];
-            })) {
-                removedIndexs.unshift(i);
-            }
-        });
+            // remove stub aliases
+            var removedIndexs = [];
+            array.forEach(require.aliases, function(al, i) {
+                if (array.some(clonedAliases, function(cAl) {
+                    return al[0].test(cAl[0]);
+                })) {
+                    removedIndexs.unshift(i);
+                }
+            });
 
-        array.forEach(removedIndexs, function(i) {
-            require.aliases.splice(i,1);
-        });
+            array.forEach(removedIndexs, function(i) {
+                require.aliases.splice(i, 1);
+            });
 
-        // clear cache again
-        require.undef(modulePath);
-        array.forEach(aliases, function (a) {
-            require.undef(a[0]);
-            require.undef(a[1]);
-        });
+            // clear cache again
+            require.undef(modulePath);
+            array.forEach(aliases, function(a) {
+                require.undef(a[0]);
+                require.undef(a[1]);
+            });
 
-        return returnModule;
-    };
-});
+            return returnModule;
+        };
+    });
